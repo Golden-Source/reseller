100d63a2c8c7c9c40121a2d75f72b754a58474f9
configserverpro <45038149+configserverpro@users.noreply.github.com>
2019-08-20 19:45:57 +0430

    bugfix#1

--- modules/servers/GoldenSource/include/bootstrap.php
+++ modules/servers/GoldenSource/include/bootstrap.php
@@ -5,3 +5,25 @@
  */
 require_once __DIR__ . "/vendor/autoload.php";
 require_once __DIR__ . "/functions.php";
+
+spl_autoload_register(function ($class_name) {
+    $class_name = str_replace('GoldenSourceUI\\', null, $class_name);
+    $class_name = str_replace('\\', DIRECTORY_SEPARATOR, $class_name);
+    $filename = dirname(dirname(dirname(__DIR__))) . '/addons/GoldenSource/include/GoldenSourceUI/' . $class_name . '.php';
+    if (is_file($filename)) {
+        require_once $filename;
+        return true;
+    }
+    return false;
+});
+
+spl_autoload_register(function ($class_name) {
+    $class_name = str_replace('GoldenSource\\', null, $class_name);
+    $class_name = str_replace('\\', DIRECTORY_SEPARATOR, $class_name);
+    $filename = __DIR__ . '/GoldenSource/' . $class_name . '.php';
+    if (is_file($filename)) {
+        require_once $filename;
+        return true;
+    }
+    return false;
+});
