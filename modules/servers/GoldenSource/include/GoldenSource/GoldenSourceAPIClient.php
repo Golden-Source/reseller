ccd3083d84c91c9d77624465f6999a5912546845
Golden-Source <>
2019-08-23 00:55:00 +0430

    Api bugfix

--- modules/servers/GoldenSource/include/GoldenSource/GoldenSourceAPIClient.php
+++ modules/servers/GoldenSource/include/GoldenSource/GoldenSourceAPIClient.php
@@ -49,7 +49,7 @@ class GoldenSourceAPIClient
      * @param $baseUrl
      * @param $userAgent
      */
-    public function __construct($apiToken, $baseUrl = 'https://GoldenSource.pro/api/v2/', $userAgent = '')
+    public function __construct($apiToken, $baseUrl = 'https://api.goldenSource.ir/', $userAgent = '')
     {
         $this->apiToken = $apiToken;
         $this->baseUrl = $baseUrl;
