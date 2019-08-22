8fb53c8581347d24605951e21903361f3120ebba
Golden-Source <>
2019-08-23 00:50:28 +0430

    Bugfix

--- modules/servers/GoldenSource/include/bootstrap.php
+++ modules/servers/GoldenSource/include/bootstrap.php
@@ -4,7 +4,7 @@
  * © All right reserved for GoldenSource Team (GoldenSource.Pro)
  */
 require_once __DIR__ . "/functions.php";
-
+ 
 spl_autoload_register(function ($class_name) {
     $class_name = str_replace('GoldenSourceUI\\', null, $class_name);
     $class_name = str_replace('\\', DIRECTORY_SEPARATOR, $class_name);
