4180c74b9d25f2c514ca54312248837c2999af1b
GoldenSource <50501111+Golden-Source@users.noreply.github.com>
2019-09-27 19:32:39 +0330

    Update GoldenSource.php

--- includes/hooks/GoldenSource.php
+++ includes/hooks/GoldenSource.php
@@ -9,7 +9,7 @@ add_hook('ClientAreaPrimaryNavbar', 1, function (MenuItem $primaryNavbar)
     global $_LANG;
     if (!is_null($primaryNavbar->getChild('Services'))) {
         $primaryNavbar->getChild('Services')->addChild('MyLicenses', array(
-            'label' => Lang::trans('myLicenses'),
+            'label' => 'لایسنس اشتراکی',
             'uri' => 'clientarea.php?action=services&module=GoldenSource',
             'order' => '10',
         ));
