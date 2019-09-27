6059d7dd73b531bd82f92f015e3beb67f7d75b85
GoldenSource <50501111+Golden-Source@users.noreply.github.com>
2019-09-27 19:35:08 +0330

    Update bootstrap.php

--- modules/addons/GoldenSource/include/bootstrap.php
+++ modules/addons/GoldenSource/include/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * GoldenSource module by Amirhossein Matini (matiniamirhossein@gmail.com) 
- * © All right reserved for GoldenSource Team (GoldenSource.Pro)
+ * © All right reserved for GoldenSource Team (GoldenSource.ir)
  */
 require dirname(dirname(dirname(__DIR__))) . '/servers/GoldenSource/include/bootstrap.php';
 function GoldenSource_getAssetPath($file = null){
